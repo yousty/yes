d08c10e00e71e3830bfc151f6682f336385d7fa4
ncri <nico.ritsche@gmail.com>
2025-03-20 16:26:25 +0000

    Add commands DSL specs / refactoring

--- yes-core/lib/yes/core/aggregate/dsl/command_definer.rb
+++ yes-core/lib/yes/core/aggregate/dsl/command_definer.rb
@@ -4,7 +4,9 @@ module Yes
   module Core
     class Aggregate
       module Dsl
-        # Factory class that creates and defines commands on aggregates
+        # Factory class that creates and defines commands on aggregates.
+        # Handles the creation and registration of all necessary command-related classes,
+        # including validation of attributes and DSL evaluation.
         #
         # @example
         #   command_data = CommandData.new(name: :assign_user, aggregate_class: Company)
@@ -22,86 +24,91 @@ module Yes
         #   end
         #
         class CommandDefiner
-          # Error raised when a command is defined with payload attributes that are not defined on the aggregate
+          # Error raised when attributes are used in the command that are not defined on the aggregate
           class UndefinedAttributeError < StandardError; end
 
-          # @return [CommandData] the data object containing command configuration
+          # @return [CommandData] The data object containing command configuration
           attr_reader :command_data
           private :command_data
 
           # Initializes a new CommandDefiner instance
           #
-          # @param command_data [CommandData] the data object containing command configuration
-          # @return [CommandDefiner] a new instance of CommandDefiner
+          # @param command_data [CommandData] The data object containing command configuration
+          # @return [CommandDefiner] A new instance of CommandDefiner
           def initialize(command_data)
             @command_data = command_data
           end
 
           # Generates and registers all necessary classes for the command.
-          # This includes command classes, event classes, and guard evaluator classes,
-          # as well as defining related methods on the aggregate class.
+          # This includes command classes, event classes, a guard evaluator class,
+          # a state updater class, as well as defining related methods on the aggregate class.
           #
           # @param block [Proc] Optional block for defining payload, guards and other command configurations
           # @return [void]
-          # @raise [UndefinedAttributeError] If the command is defined with payload attributes that are not defined on
-          # the aggregate
+          # @raise [UndefinedAttributeError] If attributes used in the command are not defined on the aggregate
           def call(&block)
-            # defines an empty guard evaluator class, because we don't have any default guards defined
-            @guard_evaluator_class = ClassResolvers::Command::GuardEvaluator.new(command_data).call
-            @state_updater_class = ClassResolvers::Command::StateUpdater.new(command_data).call
+            create_and_register_block_evaluator_classes
             evaluate_dsl_block(&block) if block
-
-            if command_data.update_state_block
-              validate_state_update_attributes!
-            else
-              validate_payload_attributes!
-            end
-
-            @command_class = ClassResolvers::Command::Command.new(command_data).call
-            @event_class = ClassResolvers::Command::Event.new(command_data).call
-            MethodDefiners::Command::Command.new(command_data).call
-            MethodDefiners::Command::CanCommand.new(command_data).call
-
+            validate_accessed_attributes
+            create_and_register_command_classes
             register_command_events
           end
 
           private
 
-          # Validates that all payload attributes are defined on the aggregate
+          # Creates and registers the guard evaluator and state updater classes
           #
           # @return [void]
-          # @raise [UndefinedAttributeError] If the command is defined with payload attributes that are not defined on
-          # the aggregate
-          def validate_payload_attributes!
-            return if command_data.payload_attributes.empty?
-
-            aggregate_attributes = command_data.aggregate_class.attributes
-            undefined_attributes = command_data.payload_attributes.keys.reject do |attr_name|
-              aggregate_attributes.key?(attr_name)
-            end
-
-            return if undefined_attributes.empty?
-
-            raise UndefinedAttributeError, "Command '#{command_data.name}' is defined with payload attributes " \
-                                           "that are not defined on the aggregate: #{undefined_attributes.join(', ')}. " \
-                                           "Please define these attributes using the 'attribute' method first."
+          def create_and_register_block_evaluator_classes
+            @guard_evaluator_class = ClassResolvers::Command::GuardEvaluator.new(command_data).call
+            @state_updater_class = ClassResolvers::Command::StateUpdater.new(command_data).call
           end
 
-          # Validates that all attributes used in the update_state block are defined on the aggregate
+          # Creates and registers command-related classes and defines their methods
           #
           # @return [void]
-          # @raise [UndefinedAttributeError] If the update_state block uses attributes that are not defined on the aggregate
-          def validate_state_update_attributes!
-            return unless @state_updater_class.updated_attributes
+          def create_and_register_command_classes
+            @command_class = ClassResolvers::Command::Command.new(command_data).call
+            @event_class = ClassResolvers::Command::Event.new(command_data).call
+            MethodDefiners::Command::Command.new(command_data).call
+            MethodDefiners::Command::CanCommand.new(command_data).call
+          end
+
+          # Validates attributes based on whether the command uses update_state or payload
+          #
+          # @return [void]
+          # @raise [UndefinedAttributeError] If any attributes are not defined on the aggregate
+          def validate_accessed_attributes
+            if command_data.update_state_block
+              validate_attributes!(
+                @state_updater_class.updated_attributes,
+                'update_state block uses attributes'
+              )
+            else
+              validate_attributes!(
+                command_data.payload_attributes.keys,
+                'payload attributes'
+              )
+            end
+          end
+
+          # Validates that all given attributes are defined on the aggregate
+          #
+          # @param attributes [Array<Symbol>] The attributes to validate
+          # @param context [String] The context in which these attributes are being used
+          # @return [void]
+          # @raise [UndefinedAttributeError] If any of the attributes are not defined on the aggregate
+          def validate_attributes!(attributes, context)
+            return if attributes.empty?
 
             aggregate_attributes = command_data.aggregate_class.attributes
-            undefined_attributes = @state_updater_class.updated_attributes.reject do |attr_name|
+            undefined_attributes = attributes.reject do |attr_name|
               aggregate_attributes.key?(attr_name)
             end
 
             return if undefined_attributes.empty?
 
-            raise UndefinedAttributeError, "Command '#{command_data.name}' update_state block uses attributes " \
+            raise UndefinedAttributeError, "Command '#{command_data.name}' #{context} " \
                                            "that are not defined on the aggregate: #{undefined_attributes.join(', ')}. " \
                                            "Please define these attributes using the 'attribute' method first."
           end
