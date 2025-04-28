8e5398d7732be367b334a55296fc7d86f4ee43ed
Konrad Badzioch <konrad.badzioch@useo.pl>
2025-04-28 08:21:00 +0000

    Add default 'assign_' command definition to 'parent' method of DSL aggregate YES-230

--- yes-core/lib/yes/core/aggregate.rb
+++ yes-core/lib/yes/core/aggregate.rb
@@ -18,7 +18,10 @@ module Yes
     #
     # @example Define an aggregate with a parent
     #   class ProfileAggregate < Yes::Core::Aggregate
-    #     parent :user
+    #     parent :user do
+    #       guard(:user_exists) { User.exists?(id: command.user_id) }
+    #       guard(:not_removed) { trashed_at.blank? }
+    #     end
     #     attribute :bio, :string
     #   end
     #
@@ -72,9 +75,16 @@ module Yes
         #
         # @param name [Symbol] The name of the parent.
         # @param options [Hash] Options for configuring the parent.
+        # @yield Block for defining guards and other attribute configurations.
         # @return [void]
-        def parent(name, options = {})
+        def parent(name, options = {}, &)
           parent_aggregates[name] = options
+
+          attribute :"#{name}_id", :uuid
+          command :"assign_#{name}" do
+            payload :"#{name}_id" => :uuid
+            instance_eval(&) if block_given?
+          end
         end
 
         # Retrieves or initializes the parent_aggregates hash.
