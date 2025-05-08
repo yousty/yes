327ed46b715956c36d3ae17dafacb430095c57af
Konrad Badzioch <konrad.badzioch@useo.pl>
2025-05-08 09:54:51 +0000

    Change way options param is defined for parent method YES-230

--- yes-core/lib/yes/core/aggregate.rb
+++ yes-core/lib/yes/core/aggregate.rb
@@ -71,13 +71,14 @@ module Yes
           end.enable
         end
 
-        # Defines a parent aggregate.
+        # Defines a parent aggregate and automatically registers a corresponding Assign command
+        # together with a corresponding attribute.
         #
         # @param name [Symbol] The name of the parent.
         # @param options [Hash] Options for configuring the parent.
         # @yield Block for defining guards and other attribute configurations.
         # @return [void]
-        def parent(name, options = {}, &)
+        def parent(name, **options, &)
           parent_aggregates[name] = options
 
           attribute :"#{name}_id", :uuid
