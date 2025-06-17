fa6c1a27634307e35c487e67dff32132b8112fb5
ncri <nico.ritsche@gmail.com>
2025-06-17 12:58:02 +0000

    wip

--- dummy/config/routes.rb
+++ dummy/config/routes.rb
@@ -3,4 +3,7 @@
 Rails.application.routes.draw do
   mount Yes::Command::Api::Engine => '/commands'
   mount Yes::Read::Api::Engine => '/queries'
+
+  require 'pg_eventstore/web'
+  mount PgEventstore::Web::Application => 'pg_eventstore'
 end
